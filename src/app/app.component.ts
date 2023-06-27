bf051fc1fe6733d1e5d212531567aa6f216fbeec
tawfik akaridan <tawfikear@gmail.com>
2023-06-28 00:19:42 +0200

    03.1 Les Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lesdirectives';
+  title: string = 'Bonjour';
+
+  isAdmin: boolean = false;
+
 }
